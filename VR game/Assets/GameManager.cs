215047bce7ed459ab019008fbbb51c0d7483fbc1
Lawrence Zhu <lawrence.zhu@coderclass.nl>
2021-12-16 09:25:17 +0100

    level is done, day/night cycle in the making

---
+++ VR game/Assets/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameParameters gameParameters;
+
+    private void Awake()
+    {
+        GetComponent<DayAndNightCycler>().enabled = gameParameters.enableDayAndNightCycle;
+    }
+
+
+}
