c41c24e597fbdc1fd1579d687d711d752fda25bb
Lawrence Zhu <lawrence.zhu@coderclass.nl>
2022-01-14 12:48:53 +0100

    door now opens when player is close, also fixed the cola can issue

---
+++ VR game/Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    public Animator animator;
+    public void OnTriggerEnter()
+    {                
+            Debug.Log("within reach");
+        animator.SetBool("character_nearby", true);
+    }
+    public void OnTriggerExit()
+    {
+        Debug.Log("lost connection");
+        animator.SetBool("character_nearby", false);
+    }
+
+}
--- VR game/Assets/SteamVR/InteractionSystem/Samples/Scripts/ButtonEffect.cs
+++ VR game/Assets/SteamVR/InteractionSystem/Samples/Scripts/ButtonEffect.cs
@@ -33,8 +33,8 @@ namespace Valve.VR.InteractionSystem.Sample
         public void Spawn()
         {
             Debug.Log("a can just spawned");
-            Instantiate(prefab, new Vector3 (0, 0, 0), Quaternion.identity);
-            
+            Instantiate(prefab, new Vector3 (10, 2, -19), Quaternion.identity);
+            System.Threading.Thread.Sleep(2000);
         }
     }
 }
